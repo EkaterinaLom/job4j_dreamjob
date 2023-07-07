3f90f8508bb798f9b466534541ad66d24fa5d4ed
EkaterinaLom <Lomakinaei@mail.ru>
2023-07-07 18:25:01 +1000

    2. Связь слоев через Spring DI. [#504856 # [#504856]]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -1,16 +1,16 @@
 package ru.job4j.dreamjob.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Vacancy;
-
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+@Repository
 public class MemoryVacancyRepository implements VacancyRepository {
 
-    private static final MemoryVacancyRepository INSTANCE = new MemoryVacancyRepository();
     private int nextId = 1;
     private final Map<Integer, Vacancy> vacancies = new HashMap<>();
 
@@ -27,10 +27,6 @@ public class MemoryVacancyRepository implements VacancyRepository {
                 LocalDateTime.now()));
         save(new Vacancy(0, "Senior Java Developer", "Description Senior Java Developer",
                 LocalDateTime.now()));
-    }
-
-    public static MemoryVacancyRepository getInstance() {
-        return INSTANCE;
     }
 
     @Override
