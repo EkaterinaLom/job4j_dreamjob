788b267bd897fc19dcb22b9cb753855e33a43654
EkaterinaLom <Lomakinaei@mail.ru>
2023-07-19 22:29:21 +1000

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859 # [#504859]]

--- src/main/java/ru/job4j/dreamjob/service/SimpleCityService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleCityService.java
@@ -12,8 +12,8 @@ public class SimpleCityService implements CityService {
 
     private final CityRepository cityRepository;
 
-    private SimpleCityService(CityRepository cityRepository) {
-        this.cityRepository = cityRepository;
+    public SimpleCityService(CityRepository sql2oCityRepository) {
+        this.cityRepository = sql2oCityRepository;
     }
 
     @Override
